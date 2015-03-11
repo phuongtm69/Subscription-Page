d4fe03e2a03c1dd75302bb8e2a0b357b5c8130a0
Tran Minh Phuong <phuongtm@Trans-MacBook-Pro.local>
2015-03-11 22:49:16 +0700

    #Update responsive template, vietnamese

--- src/java/IndexManagedBean.java
+++ src/java/IndexManagedBean.java
@@ -1,8 +1,6 @@
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import Controllers.ValidationControl;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 /**
@@ -19,9 +17,9 @@ public class IndexManagedBean {
     public IndexManagedBean() {
     }
 
-    private String email;
+    private boolean firstView = true;
 
-    private String name;
+    private String email;
 
     public String getEmail() {
         return email;
@@ -31,37 +29,29 @@ public class IndexManagedBean {
         this.email = email;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getAccess() {
-        if (name == null || email == null || name.equals("") || email.equals("")) {
-            return "Please enter username and password";
-        } else if(email.contains("@")) {
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter("user.txt"));
-                writer.write("Name: " + name + " | Email: " + email);
-                writer.close();
-            } catch (Exception e) {
-            }
-            return "<form name=\"login\" method=\"post\" action=\"http://192.168.11.1:5280/\">\n"
-                    + "\n"
-                    + "<div align=\"center\">\n"
-                    + "\n"
-                    + "  <input type=\"hidden\" name=\"accept_terms\" value=\"yes\" />   <input type=\"hidden\" name=\"redirect\" value=\"https://facebook.com\">\n"
-                    + "\n"
-                    + "  <input type=\"hidden\" name=\"mode_login\">\n"
-                    + "\n"
-                    + "  <input type=\"submit\" value=\"Click here to continue\">\n"
-                    + "\n"
-                    + "</div>";
+        if (firstView) {
+            firstView = false;
+            return "";
         }
-        return "INVALID";
+        if (email != null && !email.equals("")) {
+            if (ValidationControl.isValidEmailAddress(email)) {
+                return "<form name=\"login\" method=\"post\" action=\"http://192.168.11.1:5280/\">\n"
+                        + "\n"
+                        + "<div align=\"center\">\n"
+                        + "\n"
+                        + "  <input type=\"hidden\" name=\"accept_terms\" value=\"yes\" />   <input type=\"hidden\" name=\"redirect\" value=\"https://facebook.com\">\n"
+                        + "\n"
+                        + "  <input type=\"hidden\" name=\"mode_login\">\n"
+                        + "\n"
+                        + "  <input type=\"submit\" value=\"Truy cập\" class=\"large red button style-1\">\n"
+                        + "\n"
+                        + "</div>";
+            } else {
+                return "<center>Địa chỉ email không hợp lệ</center>";
+            }
+        }
+        return "<center>Xin vui lòng nhập địa chỉ email</center>";
     }
 
 }
